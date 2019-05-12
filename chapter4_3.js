9ec005103466344d901d39ca4ec4ed99a12609ad
phg98 <phg98@naver.com>
2019-05-12 15:17:21 +0900

    Chapter 4 - 153

--- chapter4_3.js
+++ chapter4_3.js
@@ -66,5 +66,25 @@ const func152 = () => {
     }
     crawler.crawl(parse)({q: '서울대입구역 맛집'})
 }
-func152()
+//func152()
 
+const func153 = () => {
+    const cheerio = require('cheerio');
+    const crawler = require('./crawler')
+    const fs = require('fs');
+
+    const parse = (decodedResult)=>{
+        console.log(decodedResult);
+        const $ = cheerio.load(decodedResult);
+        const titles = $('h3.r').find('a');
+        for (let i=0; i<titles.length; i += 1) {
+            const title = $(titles[i]).text();
+            console.log(title);
+        }
+        titles.each((index, title) => {
+            fs.appendFile('./titles.txt', `${$(title).text()}\n`, ()=>{})
+        })
+    }
+    crawler.crawl(parse)({q: '서울대입구역 맛집'})
+}
+func153();
